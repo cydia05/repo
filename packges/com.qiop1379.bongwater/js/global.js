996d56f45b1bac261c7e5a9fbbf66a7895786779
MKSTAFF95 <malikaddabi@gmail.com>
2020-09-19 22:31:43 +0300

    jh

---
+++ packges/com.qiop1379.bongwater/js/global.js
@@ -0,0 +1,82 @@
+
+// Function to load files (async) (bypass cross-origin [CORS] errors)
+async function _corsBypass(URL) {
+ function getData(URL) {
+     return fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(URL)}`)
+                     .then(response => {
+                         if (response.ok) return response.json()
+                         throw new Error('Network response was not ok.')
+                     });
+ }
+ var data = await getData(URL);
+ data = data.contents;
+ return data;
+}
+
+async function corsBypass(URL) {
+ if (!URL.startsWith(window.location.origin)) return await _corsBypass(URL);
+
+ return await fetch(URL).then(r => r.text());
+}
+
+//Function to set cookie
+function setCookie(name, value) {
+ var expires = "";
+ var date = new Date();
+ date.setTime(date.getTime() + 999999999);
+ expires = "; expires=" + date.toUTCString();
+ document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+//Function to get cookie
+function getCookie(name) {
+ var nameEQ = name + "=";
+ var ca = document.cookie.split(';');
+ for(var i=0;i < ca.length;i++) {
+     var c = ca[i];
+     while (c.charAt(0)==' ') c = c.substring(1,c.length);
+     if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+ }
+ return null;
+}
+
+function getIsDarkMode() {
+ if (navigator.userAgent.includes('Dark')) return true;
+ if (navigator.userAgent.includes('Pure-Black')) return true;
+ if (window.matchMedia && window.matchMedia('(prefers-color-scheme)').media !== 'not all' && window.matchMedia('(prefers-color-scheme: dark)').matches) return true;
+ if (getCookie("enableDarkMode")) return true;
+}
+
+// Set Dark Mode Cookie if non-existant
+if (!document.cookie && !getIsDarkMode()) {
+ setCookie("enableDarkMode", false)
+}// else {
+ refreshDarkMode()
+//}
+
+// Function called by buttons that toggle dark mode on/off
+function toggleDarkMode() {
+ let darkModeStatus = getCookie("enableDarkMode")
+ if (darkModeStatus) {
+     setCookie("enableDarkMode", false)
+ } else {
+     setCookie("enableDarkMode", true)
+ }
+ refreshDarkMode()
+}
+
+// Function to enable/disable Dark Mode
+function refreshDarkMode() {
+ //let darkModeStatus = getCookie("enableDarkMode")
+ let darkModeStatus = getIsDarkMode()
+ //Check Browser is not IE
+ if (navigator.userAgent.indexOf("Trident") < 0) {
+     if (darkModeStatus) {
+         document.getElementsByTagName('html')[0].classList.add("darkMode")
+     } else {
+         document.getElementsByTagName('html')[0].classList.remove("darkMode")
+     }
+ } else {
+     alert("Sorry Internet Explorer does not support this feature!")
+ }
+}
