c298eba051a79af7efde3a034535dedc4bfbffc1
MKSTAFF95 <malikaddabi@gmail.com>
2020-09-21 00:19:32 +0300

    jj

--- packges/com.shabakaty.cinemanaa/js/global.js
+++ packges/com.shabakaty.cinemanaa/js/global.js
@@ -1,4 +1,15 @@
 
+//Function to get variables from URL
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+            var pair = vars[i].split("=");
+            if(pair[0] == variable){return pair[1];}
+    }
+    return(null);
+}
+
 // Function to load files (async) (bypass cross-origin [CORS] errors)
 async function _corsBypass(URL) {
  function getData(URL) {
